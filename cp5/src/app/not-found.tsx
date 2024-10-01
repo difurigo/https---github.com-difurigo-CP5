f62ac61227dc47043854fc2dbc8b5696816f671a
difurigo <furigo9diego@gmail.com>
2024-10-01 20:16:57 -0300

    Not-found concertado
    Co-authored-by: luixvieira <henrique2.terceiro@gmail.com>
    Co-authored-by: Melpscosta <melpscosta@gmail.com>

--- cp5/src/app/not-found.tsx
+++ cp5/src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     <div>
         <h1>404: Page Not Found</h1>
         <h2>O recurso que você está tentando acessar não está disponível.</h2>
-        <Image src="../public/cachorro-404.png" alt="Página de Erro" width={200} height={50} layout="responsive"/>
+        <Image src="/cachorro-404.png" alt="Página de Erro" width={200} height={50} layout="responsive"/>
     </div>
   )
 }
