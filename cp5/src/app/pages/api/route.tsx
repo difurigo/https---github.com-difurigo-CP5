5396af54198b8c1c7da4e465e74c9045d219c96c
difurigo <furigo9diego@gmail.com>
2024-10-01 20:53:51 -0300

    Css feito
    Co-authored-by: luixvieira <henrique2.terceiro@gmail.com>
    Co-authored-by: Melpscosta <melpscosta@gmail.com>

--- cp5/src/app/pages/api/route.tsx
+++ cp5/src/app/pages/api/route.tsx
@@ -1,2 +1,8 @@
 import { NextResponse } from "next/server";
+import {promises as fs} from "fs";
 
+export async function GET() {   
+    const file = await fs.readFile(process.cwd() + 'https://api.nasa.gov/planetary/apod?api_key=eUfhGJqxodM8l2cDREp7CogOT6z5xeyzkA9eridW&date=1995-06-16','utf-8');
+    const dados = JSON.parse(file);
+    return NextResponse.json(dados);
+}
