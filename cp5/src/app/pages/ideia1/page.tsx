f8308eae21dc96320e25d452313b64421f118d40
difurigo <furigo9diego@gmail.com>
2024-10-01 22:37:00 -0300

    Page 1 alterado
    Co-authored-by: luixvieira <henrique2.terceiro@gmail.com>
    Co-authored-by: Melpscosta <melpscosta@gmail.com>

--- cp5/src/app/pages/ideia1/page.tsx
+++ cp5/src/app/pages/ideia1/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 function Ideia1() {
     return (
-        <div className="example-title">
+        <div>
         <h1 className="example-title">Ideia 1 - Catástrofes Cósmicas</h1>
-        <p className="mt-4 text-lg">
+        <p className=".example-description">
             A primeira ideia de Velikovsky propõe que a história da Terra foi marcada por catástrofes cósmicas causadas por encontros próximos com outros corpos celestes.
         </p>
         <Image src="/cachorro-404.png" alt="Página de Erro" width={200} height={50} layout="responsive"/>
