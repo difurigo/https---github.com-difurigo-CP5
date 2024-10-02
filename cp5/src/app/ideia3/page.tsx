a7c96b7c2c74aaee3504a53556cae4914263d47d
luis <henrique3.terceiro@gmail.com>
2024-10-01 22:29:07 -0300

    Arrumando as imagens

--- cp5/src/app/ideia3/page.tsx
+++ cp5/src/app/ideia3/page.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 
-function Ideia2() {
+function Ideia3() {
     return (
         <div className="idea-page text-center">
         <h1 className="text-3xl font-bold">Ideia 3 - Impactos no Passado</h1>
         <p className="mt-4 text-lg">
         A terceira ideia aborda como impactos de asteroides e outros corpos celestes podem ter sido responsáveis por grandes extinções e mudanças no desenvolvimento humano.
         </p>
-        <Image src="/cachorro-404.png" alt="Página de Erro" width={200} height={50} layout="responsive"/>
+        <Image src="/asteroides.png" alt="Página de Erro" width={200} height={50} layout="responsive"/>
         </div>
     );
 };
 
-export default Ideia2;
+export default Ideia3;
